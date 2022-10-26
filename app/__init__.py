32ca35c2a4f04efcb87ef7966fa2c3edc44d559a
Viktoriia Zolotova <viktoriiazolotova@gmail.com>
2022-10-26 11:48:09 -0700

    Add code for API read one book, non-existing book, invalid type

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    
+    from .routes import hello_world_bp
+    from .routes import books_bp
+    app.register_blueprint(hello_world_bp)
+    app.register_blueprint(books_bp)
+
+    return app
