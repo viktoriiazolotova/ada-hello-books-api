b259e5ff19f0aa146ab9114240bcec6040b53d8c
Viktoriia Zolotova <viktoriiazolotova@gmail.com>
2022-10-30 22:24:04 -0700

    Add code for API to update one book

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,24 @@ from flask import abort, Blueprint, jsonify, make_response, request
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
+#helper functions
+def validate_book(book_id):
+    #handle invalid book_id
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message": f"book {book_id} is invalid"}, 400))
+    #search for book_id in database, return book
+
+    book = Book.query.get(book_id) # return None if no book
+    
+    #return 404 for non-existing book
+    if not book:
+        abort(make_response({"message": f"book {book_id} is not found"}, 404))
+    return book
+
+
+#route functions
 @books_bp.route("", methods=['POST'])
 def create_book():
     request_body = request.get_json()
@@ -25,20 +43,6 @@ def get_all_books():
             "description": book.description})
     return jsonify(books_response)
 
-def validate_book(book_id):
-    #handle invalid book_id
-    try:
-        book_id = int(book_id)
-    except:
-        abort(make_response({"message": f"book {book_id} is invalid"}, 400))
-    #search for book_id in database, return book
-
-    book = Book.query.get(book_id) # return None if no book
-    
-    #return 404 for non-existing book
-    if not book:
-        abort(make_response({"message": f"book {book_id} is not found"}, 404))
-    return book
 
 @books_bp.route("/<book_id>", methods = ["GET"])
 def read_one_book(book_id):
@@ -47,6 +51,19 @@ def read_one_book(book_id):
         "title": book.title,
         "description": book.description}
 
+@books_bp.route("/<book_id>", methods = ["PUT"])
+def update_book(book_id):
+    book = validate_book(book_id)
+    
+    request_body = request.get_json()
+    
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+    db.session.commit()
+    return make_response(f"Book #{book_id} successfully updated", 200)
+    
+    
+    
     #Add route and function to get a single book endpoint
     #Add response to handle book in books/<book_id> route
     #Add response to handle 400 for a invalid book_id in books/<book_id> route
